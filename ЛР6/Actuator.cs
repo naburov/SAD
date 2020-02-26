351c89b4f20addc067813ce01e6bc05e00af0dd7
naburov <burov.nikita42@gmail.com>
2020-02-26 11:08:16 +0500

    Сделана ЛР6

---
+++ ЛР6/Actuator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР6
+{
+    class Actuator : SmartHouseController
+    {
+        public override int Value { get; set; }
+
+        public Actuator()
+        {
+
+        }
+    }
+}
---
+++ ЛР6/SensorProxy.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ЛР6
+{
+    class SmartHouseControllerProxy : SmartHouseController
+    {
+        public string ClientName;
+
+        private SmartHouseController controller;
+        List<string> logs = new List<string>();
+
+        public List<string> Logs => logs;
+
+        public override int Value
+        {
+            get
+            {
+                logs.Add(
+                    String.Format("Клиент с именем {0} получил значение сенсора в {1}", ClientName, DateTime.Now));
+                return controller.Value;
+            }
+            set
+            {
+
+                logs.Add(
+                    String.Format("Клиент с именем {0} записал значение сенсора в {1}", ClientName, DateTime.Now));
+                controller.Value = value;
+            }
+        }
+
+        public SmartHouseControllerProxy(SmartHouseController controller)
+        {
+            this.controller = controller;
+            controller.Value = 0;
+        }
+
+    }
+}
